5559161cc6114143fe4f35fd7fd96b48b621cf7b
sbolzli <82468008+rainscent9@users.noreply.github.com>
2021-11-27 20:24:31 +0100

    Update ksql_queries.sql

--- Kafka/ksql_queries.sql
+++ Kafka/ksql_queries.sql
@@ -1,3 +1,8 @@
+-- If reference running, hard stop, maybe require to delete the topic
+show streams;
+show tables;
+show queries;
+terminate <query ID>;
 
 -- Topic hrv_data:
 --    Session:"Core_315_185" --> KEY
@@ -6,9 +11,7 @@
 --    timestamp:"0"
 --    type:"hrv"
 --    EventTimestamp:1638003890143
-
 DROP STREAM IF EXISTS hrv_test;
-
 CREATE STREAM IF NOT EXISTS hrv_test 
   (session VARCHAR KEY,
    EventTimestamp BIGINT,
@@ -18,108 +21,17 @@ CREATE STREAM IF NOT EXISTS hrv_test
   WITH (kafka_topic='hrv_data',
         value_format='JSON');
 
+-- Topic hrv_data_forwarded:
 DROP STREAM IF EXISTS hrv_test_forwarded;
 CREATE STREAM IF NOT EXISTS hrv_test_forwarded
   WITH (kafka_topic='hrv_data_forwarded',
         value_format='JSON')
 AS SELECT session,
-   EventTimestamp AS event_timestamp ,
-   timestamp ,
-   hrv , 
-   type 
+   EventTimestamp AS event_timestamp,
+   timestamp,
+   hrv,
+   type
 FROM hrv_test
-PARTITION BY session
+-- PARTITION BY session -- Uncomment if Key is not already set in hrv_topic (from streamline)
 EMIT CHANGES;
-
--- partition by session 
-
-
--- If reference running, hard stop, maybe require to delete the topic
-show streams;
-show tables;
-show queries;
-terminate <query ID>;
-
-
-
-
-DROP STREAM IF EXISTS vehicle_tracking_sysB_s;
-CREATE STREAM IF NOT EXISTS vehicle_tracking_sysB_s 
-  (ROWKEY VARCHAR KEY,
-   system VARCHAR,
-   timestamp VARCHAR, 
-   vehicleId VARCHAR, 
-   driverId BIGINT, 
-   routeId BIGINT,
-   eventType VARCHAR,
-   latLong VARCHAR,
-   correlationId VARCHAR)
-  WITH (kafka_topic='vehicle_tracking_sysB',
-        value_format='DELIMITED');
-
-CREATE STREAM IF NOT EXISTS vehicle_tracking_refined_s 
-  WITH (kafka_topic='vehicle_tracking_refined',
-        value_format='AVRO',
-        VALUE_AVRO_SCHEMA_FULL_NAME='com.trivadis.avro.VehicleTrackingRefined')
-AS SELECT truckId AS ROWKEY
-		, 'Tracking_SysA' AS source
-		, timestamp
-		, AS_VALUE(truckId) AS vehicleId
-		, driverId
-		, routeId
-		, eventType
-		, latitude
-		, longitude
-		, correlationId
-FROM vehicle_tracking_sysA_s
-PARTITION BY truckId
-EMIT CHANGES;
-
-
-CREATE STREAM IF NOT EXISTS vehicle_tracking_refined_s 
-  WITH (kafka_topic='vehicle_tracking_refined',
-        value_format='AVRO',
-        VALUE_AVRO_SCHEMA_FULL_NAME='com.trivadis.avro.VehicleTrackingRefined')
-AS SELECT truckId AS ROWKEY
-		, 'Tracking_SysA' AS source
-		, timestamp
-		, AS_VALUE(truckId) AS vehicleId
-		, driverId
-		, routeId
-		, eventType
-		, latitude
-		, longitude
-		, correlationId
-FROM vehicle_tracking_sysA_s
-PARTITION BY truckId
-EMIT CHANGES;
-
-
-INSERT INTO vehicle_tracking_refined_s 
-SELECT ROWKEY
-    , 'Tracking_SysB' AS source
-	, timestamp
-	, vehicleId
-	, driverId
-	, routeId
-	, eventType
-	, cast(split(latLong,':')[1] as DOUBLE) as latitude
-	, CAST(split(latLong,':')[2] AS DOUBLE) as longitude
-	, correlationId
-FROM vehicle_tracking_sysB_s
-EMIT CHANGES;
-
-
-
-
-empty-0:"0"
-HRV:null
-timestamp:"0"
-type:"hrv"
-Session:"Core_315_185"
-EventTimestamp:1638003890143
-
-
-
-
 
