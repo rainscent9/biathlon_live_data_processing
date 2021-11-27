b287f95f8effd60441587510a5c4f4415d21bda5
sbolzli <82468008+rainscent9@users.noreply.github.com>
2021-11-27 20:38:50 +0100

    Update ksql_queries.sql

--- Kafka/ksql_queries.sql
+++ Kafka/ksql_queries.sql
@@ -11,8 +11,8 @@ terminate <query ID>;
 --    timestamp:"0"
 --    type:"hrv"
 --    EventTimestamp:1638003890143
-DROP STREAM IF EXISTS hrv_test;
-CREATE STREAM IF NOT EXISTS hrv_test 
+DROP STREAM IF EXISTS hrv_data_s1;
+CREATE STREAM IF NOT EXISTS hrv_data_s1
   (session VARCHAR KEY,
    EventTimestamp BIGINT,
    timestamp VARCHAR,
@@ -22,16 +22,15 @@ CREATE STREAM IF NOT EXISTS hrv_test
         value_format='JSON');
 
 -- Topic hrv_data_forwarded:
-DROP STREAM IF EXISTS hrv_test_forwarded;
-CREATE STREAM IF NOT EXISTS hrv_test_forwarded
-  WITH (kafka_topic='hrv_data_forwarded',
+DROP STREAM IF EXISTS hrv_data_s2;
+CREATE STREAM IF NOT EXISTS hrv_data_s2
+  WITH (kafka_topic='hrv_data_s2',
         value_format='JSON')
 AS SELECT session,
    EventTimestamp AS event_timestamp,
    timestamp,
    hrv,
    type
-FROM hrv_test
+FROM hrv_data_s1
 -- PARTITION BY session -- Uncomment if Key is not already set in hrv_topic (from streamline)
 EMIT CHANGES;
-
